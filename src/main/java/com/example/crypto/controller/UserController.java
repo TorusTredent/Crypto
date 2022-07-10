086e185648475ed3602ab0a3916c813c3554b4e2
TorusTredent <torus.tredent@gmail.com>
2022-07-11 01:41:14 +0300

    change mapping for methode notify

--- src/main/java/com/example/crypto/controller/UserController.java
+++ src/main/java/com/example/crypto/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class UserController {
 
     private final LoreUserService loreUserService;
 
-    @GetMapping("/user")
+    @PostMapping("/user")
     public ResponseEntity<?> notify(@RequestBody UserRegDto user) {
         User saveUser = loreUserService.notify(user);
         return new ResponseEntity<>(new UserDto(saveUser.getUsername(), saveUser.getSymbol(), saveUser.getPrice()),
